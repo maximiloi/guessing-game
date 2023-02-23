b6486c3740386161657f605c3c4268024975f569
maximiloi <maximiloi@gmail.com>
2023-02-23 22:14:56 +0300

    feat: add solutionL

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return (this.result = this.min + Math.round((this.max - this.min) / 2));
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
